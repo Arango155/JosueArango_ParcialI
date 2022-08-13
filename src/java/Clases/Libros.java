780d6bc05fb248bb85486e1e34fef355df9879a6
Arango155 <jdarango031@gmail.com>
2022-08-13 10:01:52 -0600

    Creo el paquete clases y en este creo una clase para crear el objeto libro

---
+++ src/java/Clases/Libros.java
@@ -0,0 +1,71 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package Clases;
+
+/**
+ *
+ * @author jdara
+ */
+public class Libros {
+    
+    private String code;
+    private String name;
+    private String paste;
+    private String editorial;
+    private String date;
+    
+    public Libros (String code, String name, String paste, String editorial, String date){
+    
+        this.code=code;
+        this.name=name;
+        this.paste=paste;
+        this.editorial=editorial;
+        this.date=date;
+    
+    }
+
+    public String getCode() {
+        return code;
+    }
+
+    public void setCode(String code) {
+        this.code = code;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getPaste() {
+        return paste;
+    }
+
+    public void setPaste(String paste) {
+        this.paste = paste;
+    }
+
+    public String getEditorial() {
+        return editorial;
+    }
+
+    public void setEditorial(String editorial) {
+        this.editorial = editorial;
+    }
+
+    public String getDate() {
+        return date;
+    }
+
+    public void setDate(String date) {
+        this.date = date;
+    }
+    
+    
+    
+}
